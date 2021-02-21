136a36d8c1d81e6fb3cc2cd19832ac8d1f99dbb7
Sugiarto Cokrowibowo <sugiarto.cokrowibowo.id@gmail.com>
2021-02-21 20:22:11 +0800

    new commit

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -11,6 +11,6 @@ package fosalgo;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sugiarto Cokrowibowo");
+        System.out.println("Sugiarto Cokrowibowo...");
     }
 }
